4b62492d950c8055726ce47773ea151cfc39e050
Sanglae Kim <nova0302@hotmail.com>
2021-12-29 22:01:01 +0900

    pwm with adc dma triggerd by tim6 added

---
+++ l0_pwm_with_adc_dam_tim6_trig/App/App.c
@@ -0,0 +1,62 @@
+/*
+ * App.c
+ *
+ *  Created on: Dec 25, 2021
+ *      Author: LG
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "main.h"
+#include "App.h"
+#include "lcd_user.h"
+
+extern ADC_HandleTypeDef hadc;
+extern I2C_HandleTypeDef hi2c1;
+extern TIM_HandleTypeDef htim6;
+extern TIM_HandleTypeDef htim2;
+extern TIM_HandleTypeDef htim22;
+
+//https://blog.naver.com/eziya76/221472568302
+#define N_ADC_CHANNEL (2)
+
+static uint32_t adcValues[N_ADC_CHANNEL];
+volatile bool updateAdcValues = false;
+
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc) {
+	updateAdcValues = true;
+	HAL_GPIO_TogglePin(LED3_GPIO_Port, LED3_Pin);
+}
+
+void appSetup() {
+	HAL_ADC_Start_DMA(&hadc, adcValues, N_ADC_CHANNEL);
+
+	HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
+	HAL_TIM_PWM_Start(&htim22, TIM_CHANNEL_2);
+
+	HAL_TIM_Base_Start(&htim6);
+	lcd_init(&hi2c1);
+}
+void appLoop() {
+
+	static uint32_t last;
+	uint32_t now = HAL_GetTick();
+
+	if (now - last > 1000) {
+		HAL_GPIO_TogglePin(LED1_GPIO_Port, LED1_Pin);
+		last = now;
+		OLED_Clear();
+		for (int i = 0; i < N_ADC_CHANNEL; i++) {
+			char pStr[40] = { 0, };
+			sprintf(pStr, "%d: %d", i, (int) adcValues[i]);
+			OLED_ShowString(0, i, pStr, 1);
+		}
+	}
+
+	if (updateAdcValues) {
+		updateAdcValues = false;
+		htim2.Instance->CCR1  = adcValues[0] * 1000 / 4096;
+		htim22.Instance->CCR2 = adcValues[1] * 1000 / 4096;
+	}
+}
